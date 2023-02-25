1ddbcc1339ae6446469314dcc78abb526e38e6c1
krishnagond <55579055+krishnagond@users.noreply.github.com>
2023-02-25 14:26:06 +0530

    Amazoaff

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 @Service
 public class OrderService {
-    @Autowired
-    OrderRepository orderRepository;
+    //@Autowired
+    OrderRepository orderRepository = new OrderRepository();
     public String addOrder(Order orderId){
         String result = orderRepository.addOrder(orderId);
         return result;
